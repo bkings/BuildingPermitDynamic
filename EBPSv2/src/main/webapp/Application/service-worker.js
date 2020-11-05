be6f2cd0b165962885b1bfacdc6cc67b9e797f87
bkings <bbikings@yahoo.com>
2020-11-05 10:11:15 +0545

    build

--- EBPSv2/src/main/webapp/Application/service-worker.js
+++ EBPSv2/src/main/webapp/Application/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e17f3a2093a2ae69c2a9fb52faa06f96.js"
+  "./precache-manifest.b8ea10752afd9e3595fb36d49dfbf000.js"
 );
 
 self.addEventListener('message', (event) => {
