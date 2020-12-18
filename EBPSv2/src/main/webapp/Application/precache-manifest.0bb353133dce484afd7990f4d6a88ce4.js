348095c80a4e9c9cc1f22dc33de5590ef4f9a5a6
bkings <bbikings@yahoo.com>
2020-12-18 16:36:01 +0545

    build

---
+++ EBPSv2/src/main/webapp/Application/precache-manifest.0bb353133dce484afd7990f4d6a88ce4.js
@@ -0,0 +1,278 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64557a25e32d0782f4e24fd53c165e5d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "78e255f16bcb2ef535a8",
+    "url": "./static/css/5.28af53cb.chunk.css"
+  },
+  {
+    "revision": "dcd0962f588027495ca2",
+    "url": "./static/css/6.b585b80f.chunk.css"
+  },
+  {
+    "revision": "468c15820c9899913b72",
+    "url": "./static/css/main.71561452.chunk.css"
+  },
+  {
+    "revision": "dc87b28d20f216151765",
+    "url": "./static/js/0.0f08dfc6.chunk.js"
+  },
+  {
+    "revision": "416f9c83343778063620",
+    "url": "./static/js/1.73d09769.chunk.js"
+  },
+  {
+    "revision": "15c55c4193d130aed08e",
+    "url": "./static/js/10.7ac30938.chunk.js"
+  },
+  {
+    "revision": "906057da9501384d03b2",
+    "url": "./static/js/11.f2532529.chunk.js"
+  },
+  {
+    "revision": "7288b0c82457b0343855",
+    "url": "./static/js/12.fff4d727.chunk.js"
+  },
+  {
+    "revision": "badf4d72133ec5605998",
+    "url": "./static/js/13.8363cbc4.chunk.js"
+  },
+  {
+    "revision": "e12e3f17ea022c4716a6",
+    "url": "./static/js/14.489a9a97.chunk.js"
+  },
+  {
+    "revision": "c2330b5460d02362ce5d",
+    "url": "./static/js/15.a08c37af.chunk.js"
+  },
+  {
+    "revision": "b7fb4177364068429c01",
+    "url": "./static/js/16.cb6ba735.chunk.js"
+  },
+  {
+    "revision": "7d8756adcafd5dfe3e84",
+    "url": "./static/js/17.0936c083.chunk.js"
+  },
+  {
+    "revision": "903aac30b0f256fa87ad",
+    "url": "./static/js/18.27ce6023.chunk.js"
+  },
+  {
+    "revision": "40f3ae3286f64fc9efe0",
+    "url": "./static/js/19.51db0927.chunk.js"
+  },
+  {
+    "revision": "5300bbfc3ca2ffc586ff",
+    "url": "./static/js/2.f0c13a52.chunk.js"
+  },
+  {
+    "revision": "c2e1d836be2ddb863c08",
+    "url": "./static/js/20.0565a4d4.chunk.js"
+  },
+  {
+    "revision": "c6c55f579bf9362c4f28",
+    "url": "./static/js/21.3e959d48.chunk.js"
+  },
+  {
+    "revision": "87a3c05b1baf98bfca68",
+    "url": "./static/js/22.417da570.chunk.js"
+  },
+  {
+    "revision": "7c037c7f9b925164fc7c",
+    "url": "./static/js/23.c050325e.chunk.js"
+  },
+  {
+    "revision": "78e255f16bcb2ef535a8",
+    "url": "./static/js/5.7f493616.chunk.js"
+  },
+  {
+    "revision": "dcd0962f588027495ca2",
+    "url": "./static/js/6.221d094c.chunk.js"
+  },
+  {
+    "revision": "b754b4de63bb1a712a65",
+    "url": "./static/js/7.2d6e0d73.chunk.js"
+  },
+  {
+    "revision": "f6c4efc1f6bdc2ea70af",
+    "url": "./static/js/8.729ee213.chunk.js"
+  },
+  {
+    "revision": "ff0e5803e3f36bade4ae",
+    "url": "./static/js/9.6a0f64e1.chunk.js"
+  },
+  {
+    "revision": "468c15820c9899913b72",
+    "url": "./static/js/main.2b27d6b6.chunk.js"
+  },
+  {
+    "revision": "6292953079209958f9ae",
+    "url": "./static/js/runtime~main.afbec3af.js"
+  },
+  {
+    "revision": "002de52cc1eece5d729feb122fa4919c",
+    "url": "./static/media/EBPS.002de52c.jpg"
+  },
+  {
+    "revision": "227d1939ca8dabb3d6a8e0f8a4093c6e",
+    "url": "./static/media/Kalimati.227d1939.ttf"
+  },
+  {
+    "revision": "9ca517a706c8db3b18daff958f140e80",
+    "url": "./static/media/Kalimati.9ca517a7.woff"
+  },
+  {
+    "revision": "b70a24af0736caf9498b561395dbbf21",
+    "url": "./static/media/Kalimati.b70a24af.eot"
+  },
+  {
+    "revision": "c34027b4ae2fbac5e1d4e695fee2051e",
+    "url": "./static/media/Kalimati.c34027b4.svg"
+  },
+  {
+    "revision": "eaefe7040bfd637af35729b0964eef49",
+    "url": "./static/media/MNEBPSflowDiagram.eaefe704.webp"
+  },
+  {
+    "revision": "bf7154eebb5a7342a1e4bb27ab3071fa",
+    "url": "./static/media/Nepal_sarkar_logo.bf7154ee.webp"
+  },
+  {
+    "revision": "6df18030b1c3b6f9beac45e9efbced1d",
+    "url": "./static/media/NotoSerifDevanagari-Regular.6df18030.ttf"
+  },
+  {
+    "revision": "1518c31b0df74c7c9bf61969574da297",
+    "url": "./static/media/ProximaNova-Bold.1518c31b.ttf"
+  },
+  {
+    "revision": "24dd8f429d02bdb53538815905089f91",
+    "url": "./static/media/ProximaNova-Bold.24dd8f42.woff"
+  },
+  {
+    "revision": "a82383c16979bba3f9e0e4c31a70e692",
+    "url": "./static/media/ProximaNova-Bold.a82383c1.eot"
+  },
+  {
+    "revision": "c7146c861c08bb3f6ee3f70af57c218d",
+    "url": "./static/media/ProximaNova-Bold.c7146c86.svg"
+  },
+  {
+    "revision": "067b1441c21c229341467c72888a836f",
+    "url": "./static/media/ProximaNova-Regular.067b1441.ttf"
+  },
+  {
+    "revision": "1953b4d399d1e4336922141a6605be6f",
+    "url": "./static/media/ProximaNova-Regular.1953b4d3.svg"
+  },
+  {
+    "revision": "4b7bc2c631a9a51726118c609822562d",
+    "url": "./static/media/ProximaNova-Regular.4b7bc2c6.eot"
+  },
+  {
+    "revision": "74139e3d3c243c92b1d31ed4ec596f6e",
+    "url": "./static/media/ProximaNova-Regular.74139e3d.woff"
+  },
+  {
+    "revision": "7264b615c7c790c090293c78dfa65cf1",
+    "url": "./static/media/ProximaNova-Semibold.7264b615.ttf"
+  },
+  {
+    "revision": "741d595e8370820afb29ca6dc1db8e11",
+    "url": "./static/media/ProximaNova-Semibold.741d595e.eot"
+  },
+  {
+    "revision": "834fb20b32b767ec602e82d7a94f8b32",
+    "url": "./static/media/ProximaNova-Semibold.834fb20b.svg"
+  },
+  {
+    "revision": "a2eed25f144ef1e958baa762a269d446",
+    "url": "./static/media/ProximaNova-Semibold.a2eed25f.woff"
+  },
+  {
+    "revision": "f418b43952eaaddaa53ae45db54e8256",
+    "url": "./static/media/Shangrila Numeric Regular.f418b439.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "./static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "./static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "./static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "./static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "./static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "./static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "./static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "./static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "./static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "./static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "./static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "87e58db76d0b0e586581fccd1dc60d7b",
+    "url": "./static/media/illam_logo.87e58db7.png"
+  },
+  {
+    "revision": "076dc20edf52be6efbb83c7dd09b84fa",
+    "url": "./static/media/map-hue.076dc20e.png"
+  },
+  {
+    "revision": "b01e6120d05abd33918d1dbb78c0660f",
+    "url": "./static/media/map-saturation-overlay.b01e6120.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "./static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "./static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "./static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "./static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "./static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ede71849659911300be77523f48c48d2",
+    "url": "./static/media/pdf.ede71849.svg"
+  }
+]);
--- EBPSv2/src/main/webapp/Application/service-worker.js
+++ EBPSv2/src/main/webapp/Application/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a1abf7f631d10bdafc0d6f0e563b649b.js"
+  "./precache-manifest.0bb353133dce484afd7990f4d6a88ce4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
